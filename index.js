09cf6923cf3ddcb34203012deb0e819776f86019
anismemon <58182830+anismemon@users.noreply.github.com>
2020-02-09 22:43:12 -0500

    mostly finished: optimied guessing function, created faulty loop, added variable guessing range, did not add cheat check

--- index.js
+++ index.js
@@ -1,37 +1,67 @@
 const readline = require('readline');
 const rl = readline.createInterface(process.stdin, process.stdout);
 
+// functions are here
+
 function ask(questionText) {
   return new Promise((resolve, reject) => {
     rl.question(questionText, resolve);
   });
 }
 
-function randomInt(max, min) {
-  return Math.floor(min + (Math.random() * (max - min + 1)))
+function binarySearch(min, max) {
+  return Math.floor((max + min) / 2);
 
 }
 start();
 
 async function start() {
-  console.log("Let's play a game where you (the human) think of a number between 1 and 100 and I (the computer) try to guess it.")
-  let answerReady = await ask("Press ENTER when you're ready.");
-  let firstGuess = '';
-  if (answerReady === "") {
-    // let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-    // console.log('You entered: ' + secretNumber);
-    firstGuess = await ask("Is it ... " + randomInt(1, 100) + "? (Y/N) ")
-  } else {
-    firstGuess = await ask("Is it ... " + randomInt(1, 100) + "? (Y/N) ")
-  } if (firstGuess.toUpperCase() === "Y" || firstGuess.toUpperCase() === "YES") {
-    console.log("Great job! You guessed it on the first try!")
+  console.log("Let's play a game where you (the human) think of a number between 1 and n and I (the computer) try to guess it.")
+
+  await ask("Press ENTER when you're ready. ");
+
+  // variables within the async function are here
+
+  let b = parseInt(await ask("Great. Please give me the upper range for my guesses. "));
+  
+  // I tried to set the default here, but it doesn't work
+  // if (b !== Number) {
+  //   b = 100;
+  // }
+
+  let a = 1;
+  let firstGuess = binarySearch(a, b);
+  let secondGuess = '';
+  let answerYesNo = "";
+  let answerHighLow = "";
+  let min = Math.min(a, b);
+  let max = Math.max(a, b);
+  let count = 1;
+
+  console.log("Now I'll guess a number between 1 and " + b + ".")
+  answerYesNo = await ask("Is it ... " + firstGuess + "? (Y/N) ")
+
+  // guessing loop here but it does not work correctly for all numbers and I don't know why
+
+  while (answerYesNo.toUpperCase() === "N" || answerYesNo.toUpperCase() === "NO") {
+    answerHighLow = await ask("Is it higher (H) or lower (L)? ")
+
+    if (answerHighLow.toUpperCase() === "H") {
+      min = Math.max(a, firstGuess)
+      a = secondGuess;
+
+    } else {
+      max = Math.min(b, firstGuess)
+      b = secondGuess;
+
+    } secondGuess = binarySearch(min, max)
+    answerYesNo = await ask("Is it ... " + secondGuess + "? (Y/N) ")
+    count += 1;
+    firstGuess = secondGuess;
+
   }
-  // Now try and complete the program.
-
-
-  // Added in my randomn number function with variable min / max
+  console.log("Wow! I guessed it in " + count + " tries!")
 
   process.exit();
-
 
 }
